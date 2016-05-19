71f8061d41fd070a169aee3ea8fd178880511c2d
Camilo Roca <carocad@unal.edu.co>
2016-05-20 00:35:16 +0200

    criterium added for performance measuring

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:uberjar {:aot :all}
+    :dev {:dependencies [; generate test data
+                         [org.clojure/test.check "0.9.0"]
+                         ; profiler
+                         [criterium "0.4.4"]]}})
