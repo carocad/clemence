ce5d1dc264101031bd7219b47001406228b24d4d
Camilo Roca <carocad@unal.edu.co>
2016-06-09 16:19:02 +0200

    test.check added as dep in dev mode. version bumped

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clemence "0.1.0-SNAPSHOT"
+(defproject clemence "0.2.0"
   :description "Fast and incremental Levenshtein distance"
   :url "https://github.com/carocad/clemence"
   :license {:name "LGPLv3"
             :url "https://github.com/carocad/clemence/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]]}}) ; profiler
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
+                                  [criterium "0.4.4"]]}}) ; profiler
