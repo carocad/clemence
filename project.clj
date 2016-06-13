1d0dadf0ff089c2f2a77d1647a58cfbbb4c3f158
Camilo Roca <carocad@unal.edu.co>
2016-06-13 23:33:39 +0200

    fast-zip added

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/carocad/clemence"
   :license {:name "LGPLv3"
             :url "https://github.com/carocad/clemence/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [fast-zip "0.7.0"]]
 
   :plugins [[perforate "0.3.4"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
