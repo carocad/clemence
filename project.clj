f3e31d93f5fdc5ceae9260fae9d02c7cb4799c59
Camilo Roca <carocad@unal.edu.co>
2016-06-17 14:25:09 +0200

    bumped project version

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
-(defproject clemence "0.2.2"
+(defproject clemence "0.3.0"
   :description "Fast and incremental Levenshtein and Longest Common Subsequence computation"
   :url "https://github.com/carocad/clemence"
   :license {:name "LGPLv3"
             :url "https://github.com/carocad/clemence/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [fast-zip "0.7.0"]]
-
   :plugins [[jonase/eastwood "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [criterium "0.4.4"]]
-                   :source-paths ["src/"] ;;perforate
-                   }})
+                                  [criterium "0.4.4"]]}})
