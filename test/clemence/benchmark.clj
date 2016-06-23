72fdc543e49695579113a3b57791ce8d8aa340dc
Camilo Roca <carocad@unal.edu.co>
2016-06-23 19:11:50 +0200

    benchmark switched to manual mode. benchmark.txt now its according to the manual mode bench replaced by quick-bench to ease benchmarking

--- test/clemence/benchmark.clj
+++ test/clemence/benchmark.clj
@@ -12,40 +12,40 @@
 (println "Goal: fuzzy search, naive --all-results")
 
 (println "case: levenshtein")
-(crit/bench (run! identity (clemence/levenshtein trie "diff")))
+(crit/quick-bench (run! identity (clemence/levenshtein trie "diff")))
 
 (println "case: longest-common-subsequence")
-(crit/bench (run! identity (clemence/lcs trie "diff")))
+(crit/quick-bench (run! identity (clemence/lcs trie "diff")))
 
 (println "===================================================================")
 
 (println "Goal: fuzzy search, naive -- 10 results")
 
 (println "case: levenshtein")
-(crit/bench (run! identity (take 10 (clemence/levenshtein trie "diff"))))
+(crit/quick-bench (run! identity (take 10 (clemence/levenshtein trie "diff"))))
 
 (println "case: longest-common-subsequence")
-(crit/bench (run! identity (take 10 (clemence/lcs trie "diff"))))
+(crit/quick-bench (run! identity (take 10 (clemence/lcs trie "diff"))))
 
 (println "===================================================================")
 
 (println "Goal: fuzzy search, constrained --all-results")
 
 (println "case: levenshtein")
-(crit/bench (run! identity (clemence/levenshtein trie "diff" 2)))
+(crit/quick-bench (run! identity (clemence/levenshtein trie "diff" 2)))
 
 (println "case: longest-common-subsequence")
-(crit/bench (run! identity (clemence/lcs trie "diff" 7)))
+(crit/quick-bench (run! identity (clemence/lcs trie "diff" 7)))
 
 (println "===================================================================")
 
 (println "Goal: fuzzy search, constrained -- 10 results")
 
 (println "case: levenshtein")
-(crit/bench (run! identity (take 10 (clemence/levenshtein trie "diff" 2))))
+(crit/quick-bench (run! identity (take 10 (clemence/levenshtein trie "diff" 2))))
 
 (println "case: longest-common-subsequence")
-(crit/bench (run! identity (take 10 (clemence/lcs trie "diff" 7))))
+(crit/quick-bench (run! identity (take 10 (clemence/lcs trie "diff" 7))))
 
 ))
 
